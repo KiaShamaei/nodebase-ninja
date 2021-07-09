5d2447a05162c792831fed51e1991875bb780b14
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-07-09 10:17:29 +0430

    add routing to the base config

--- server.js
+++ server.js
@@ -7,17 +7,31 @@ const http = require('http');
 const server = http.createServer ((req,res)=>{
 	res.setHeader ('Content-Type','text/html')
 	let url = req.url;
-	
+	let path = "./views/"
+//this is router switch	
 	switch(url){
 		case "/":
-			res.write("hello world!")
+			path +="index.html"
 			break;
 		case "/about":
-			res.write("<h2>THis is About page...</h2>")
+			path += "about.html"
 			break;
+		case "/product":
+			path += "product.html"
+			break;
+		default :
+		path += "404.html"
 		
 	}
-	res.end()
+	fs.readFile(path , (err,data)=>{
+		if(err){
+			console.log(err)
+			res.end();
+		}else{
+			res.write(data)
+			res.end()
+		}
+	})
 })
 server.listen(8000, 'localhost' , ()=>{
 	console.log('Listening for request')

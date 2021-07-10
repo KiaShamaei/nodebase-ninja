bc9d28f44a6a17a79fedf50d58ae942f05d62b30
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-07-10 07:37:56 +0430

    add status code of response to router

--- server.js
+++ server.js
@@ -4,35 +4,42 @@ const fs = require("fs");
 const http = require('http');
 
 
-const server = http.createServer ((req,res)=>{
-	res.setHeader ('Content-Type','text/html')
+const server = http.createServer((req,res) => {
+	res.setHeader('Content-Type','text/html')
 	let url = req.url;
 	let path = "./views/"
-//this is router switch	
-	switch(url){
+	//this is router switch	
+	switch (url) {
 		case "/":
-			path +="index.html"
+			path += "index.html"
+			res.statusCode = 200
 			break;
 		case "/about":
-			path += "about.html"
+			path += "about.html";
+			res.statusCode = 200;
 			break;
 		case "/product":
-			path += "product.html"
+			path += "product.html";
+			res.statusCode = 200;
 			break;
-		default :
-		path += "404.html"
-		
+		default:
+			path += "404.html";
+			res.statusCode= 404;
+
 	}
-	fs.readFile(path , (err,data)=>{
-		if(err){
+	fs.readFile(path,(err,data) => {
+		if (err) {
 			console.log(err)
 			res.end();
-		}else{
+		} else {
+			
 			res.write(data)
 			res.end()
 		}
 	})
+
+
 })
-server.listen(8000, 'localhost' , ()=>{
+server.listen(8000,'localhost',() => {
 	console.log('Listening for request')
 })

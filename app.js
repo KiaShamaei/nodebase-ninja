a11eb48aceda2a0461e5cfe16e16af1ae3ebb36e
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-07-12 21:49:12 +0430

    add partial and css

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
-const express = require("express")
+const express = require('express');
 
+// express app
+const app = express();
 
-//express app 
-const app = express()
-app.set("view engine" , "ejs")
-const blogs = [
-    {title : "First Blog" , snnipts : "lorem ipsum 1 kiarash is bala pesar"},
-    {title : "Second Blog" , snnipts : "lorem ipsum 2 Mohamad is bala pesar"},
-    {title : "Third Blog" , snnipts : "lorem ipsum 3 Peyman is bala pesar"},
+// listen for requests
+app.listen(3000);
 
-]
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
 
-//listen for requests
-app.listen(8000);
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
 
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
-app.get("/" , (req , res)=>{
-    // res.send("<p>this is home page</p>")
-    // res.sendFile('./views/index.html' , {root: __dirname})
-    //with esj doesnt need to add root address as obj
-    res.render("index" , {
-        title :  "Home" , 
-        blogs
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
-})
-     
-})
-app.get("/about" , (req,res)=>{
-    res.render("about" , {title : "About"})
-})
-
-//redirects
-// app.get ('/about-us', (req,res)=>{
-//     res.redirect('/about')
-// })
-//404 page in express 
-app.use((req, res)=>{
-    res.render('404' , {title : "404"})
-})
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});

4d6ebec818b155c8d32c8bae1811de7dc87528df
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2021-07-18 18:57:10 +0430

    add crud

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose =require('mongoose');
-const Blog = require('./models/blog')
+const Blog = require('./models/blog');
+const { result } = require('lodash');
 // express app
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/test",{useNewUrlParser : true , useUnifiedTopology : true})
 .then((result)=>{
   console.log("connected")
   //this make sense to be listen after connection establish ... 
-  app.listen(3000)
+  app.listen(3001)
 })
 .catch((err)=>{
   console.log("err in  connection !")
@@ -34,10 +35,14 @@ app.set('view engine', 'ejs');
 // 	next();
 // })
 
+
+
+//middleaware & static file -------------------
 //add morgan to work as logger ----------------
 
 app.use(morgan("tiny"))
 app.use(express.static('public'))
+app.use(express.urlencoded({extended : true}))
 // //add sandbox and mongo routes
 // app.get("/add-get" , (req,res)=>{
 //   const blog = new Blog({
@@ -76,15 +81,59 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
 res.redirect('/blogs')
 });
+app.get('/blogs', (req,res)=>{
+  Blog.find().sort({ createdAt : -1 })
+  .then((result)=>{
+    console.log(result)
+    res.render("index", { title: 'All Blogs' , blogs : result})
+
+
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+})
+app.post('/blogs' , (req,res)=>{
+  console.log(req.body)
+  const blog = new Blog(req.body)
+  blog.save()
+  .then((result)=>{
+    res.redirect('/blogs')
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+
+})
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'CreateBlog' });
+});
+app.get("/blogs/:id" , (req, res)=>{
+  const id = req.params.id
+  Blog.findById(id)
+  .then(result =>{
+    res.render("details" , {title :"Details",blog : result})
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+app.delete('/blogs/:id', (req,res)=>{
+  const id = req.params.id
+  Blog.findByIdAndDelete(id)
+  .then(result=>{
+    console.log(result)
+    res.json({redirect : "/blogs"})
+  
+  })
+  .catch(err=>{
+    console.log( err )
+  })
+})
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
-
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
